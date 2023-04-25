83a9f82697225f4fc597e48110247e08c6dced39
briofy-mammad <m.ranjbar@briofy.net>
2023-04-25 13:27:59 +0330

    fix change input

--- pages/pricing.vue
+++ pages/pricing.vue
@@ -22,7 +22,6 @@
               v-model="activeBtn"
               type="checkbox"
               class="sr-only peer outline-none"
-              @input="changeInput"
             />
             <div
               class="w-10 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"
@@ -58,7 +57,9 @@
                   : pricingCardItem.price)
               }}
             </span>
-            <span class="text-gray-500 dark:text-gray-400">/month</span>
+            <span class="text-gray-500 dark:text-gray-400"
+              >/{{ activeBtn === true ? "Yearly" : "Monthly" }}</span
+            >
           </div>
           <p class="font-light text-gray-500 sm:text-lg dark:text-gray-300">
             {{ pricingCardItem.subtitle }}
