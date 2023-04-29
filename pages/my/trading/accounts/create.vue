f861b037d2f18c063521da846994f1fc46d66db2
briofy-parsajiravand <p.jiravand@briofy.net>
2023-04-29 18:56:24 +0330

    setup create trading accounts and validations

---
+++ pages/my/trading/accounts/create.vue
@@ -0,0 +1,478 @@
+<template>
+  <section class="bg-white dark:bg-gray-900">
+    <div class="py-8 px-4 mx-auto">
+      <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">
+        Add a new event
+      </h2>
+      <VForm
+        id="products_create"
+        class="form"
+        novalidate
+        @submit="submitForm()"
+        :validation-schema="form"
+      >
+        <div
+          class="gap-4 sm:grid sm:grid-cols-2 xl:grid-cols-3 sm:gap-6 sm:mb-2"
+        >
+          <div class="mb-4 space-y-4 xl:col-span-2">
+            <div class="flex flex-row gap-4">
+              <div class="md:basis-1/2">
+                <label for="title" class="custom-input-label">Title</label>
+                <Field
+                  type="text"
+                  name="title"
+                  class="custom-input"
+                  v-model="model.title"
+                />
+                <div class="text-red-500 text-sm p-1">
+                  <ErrorMessage name="title" />
+                </div>
+              </div>
+              <div class="md:basis-1/2">
+                <label for="title" class="custom-input-label">Type</label>
+                <Field
+                  as="select"
+                  name="type"
+                  class="custom-input"
+                  v-model="model.type"
+                >
+                  <option
+                    :value="item.value"
+                    v-for="(item, index) in types"
+                    :key="index"
+                  >
+                    {{ item.name }}
+                  </option>
+                </Field>
+                <div class="text-red-500 text-sm p-1">
+                  <div class="fv-help-block">
+                    <ErrorMessage name="type" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <label for="slogan" class="custom-input-label">Slogan</label>
+              <Field
+                type="text"
+                name="slogan"
+                class="custom-input"
+                v-model="model.slogan"
+              />
+              <div class="text-red-500 text-sm p-1">
+                <ErrorMessage name="slogan" />
+              </div>
+            </div>
+            <div>
+              <label for="description" class="custom-input-label"
+                >Introduction</label
+              >
+              <Field
+                type="text"
+                name="introduction"
+                class="custom-input"
+                v-model="model.introduction"
+              />
+              <div class="text-red-500 text-sm p-1">
+                <ErrorMessage name="introduction" />
+              </div>
+            </div>
+            <div>
+              <label for="description" class="custom-input-label"
+                >Description</label
+              >
+              <Field
+                type="text"
+                name="description"
+                class="custom-input"
+                v-model="model.description"
+              />
+              <div class="text-red-500 text-sm p-1">
+                <ErrorMessage name="description" />
+              </div>
+            </div>
+
+            <div class="flex flex-row gap-4">
+              <div class="flex basis-1/2 items-center justify-center">
+                <label
+                  for="dropzone-file"
+                  class="flex flex-col items-center justify-center w-full h-32 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+                >
+                  Logo
+                  <div
+                    class="flex flex-col items-center justify-center pt-5 pb-6"
+                    v-show="!model.logo"
+                  >
+                    <svg
+                      aria-hidden="true"
+                      class="w-10 h-10 mb-3 text-gray-400"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+                      ></path>
+                    </svg>
+                    <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                      <span class="font-semibold">Click to upload</span> or drag
+                      and drop
+                    </p>
+                    <p class="text-xs text-gray-500 dark:text-gray-400">
+                      SVG, PNG, JPG or GIF (MAX. 5 MB)
+                    </p>
+                  </div>
+                  <input
+                    id="dropzone-file"
+                    type="file"
+                    class="hidden"
+                    @change="onFileChange($event)"
+                  />
+                </label>
+              </div>
+              <div class="flex basis-1/2 items-center justify-center">
+                <label
+                  for="dropzone-file"
+                  class="flex flex-col items-center justify-center w-full h-32 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+                >
+                  Cover
+                  <div
+                    class="flex flex-col items-center justify-center pt-5 pb-6"
+                    v-show="!model.logo"
+                  >
+                    <svg
+                      aria-hidden="true"
+                      class="w-10 h-10 mb-3 text-gray-400"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+                      ></path>
+                    </svg>
+                    <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                      <span class="font-semibold">Click to upload</span> or drag
+                      and drop
+                    </p>
+                    <p class="text-xs text-gray-500 dark:text-gray-400">
+                      SVG, PNG, JPG or GIF (MAX. 5 MB)
+                    </p>
+                  </div>
+                  <input
+                    id="dropzone-file"
+                    type="file"
+                    class="hidden"
+                    @change="onFileChange($event)"
+                  />
+                </label>
+              </div>
+            </div>
+            <div class="flex flex-row gap-4"></div>
+          </div>
+          <div class="mb-4 space-y-4">
+            <div>
+              <label for="add-guests" class="custom-input-label"
+                >Add guests</label
+              >
+              <div class="relative">
+                <input
+                  type="text"
+                  id="add-guests"
+                  class="block p-3 pl-4 w-full text-sm text-gray-900 bg-gray-50 rounded border outline-none border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  placeholder="Add guest email"
+                  required
+                />
+                <button
+                  type="button"
+                  class="text-white absolute inline-flex items-center right-2.5 bottom-2.5 bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded text-xs px-3 py-1.5 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  <svg
+                    class="w-4 h-4 -ml-0.5 mr-0.5"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  Add
+                </button>
+              </div>
+            </div>
+            <div class="items-center md:flex md:space-x-4">
+              <div class="flex flex-shrink-0 mb-4 -space-x-4 md:mb-0">
+                <img
+                  class="w-8 h-8 rounded-full border-2 border-white dark:border-gray-800"
+                  src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/helene-engels.png"
+                  alt="Helene Engels"
+                />
+                <img
+                  class="w-8 h-8 rounded-full border-2 border-white dark:border-gray-800"
+                  src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/robert-brown.png"
+                  alt="Robert Brown"
+                />
+                <img
+                  class="w-8 h-8 rounded-full border-2 border-white dark:border-gray-800"
+                  src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png"
+                  alt="Bonnie Green"
+                />
+                <a
+                  class="flex justify-center items-center w-8 h-8 text-xs font-medium text-white bg-gray-700 rounded-full border-2 border-white hover:bg-gray-600 dark:border-gray-800"
+                  href="#"
+                  >+9</a
+                >
+              </div>
+              <button
+                type="button"
+                class="inline-flex items-center py-2 px-3 text-xs font-medium text-gray-900 bg-white rounded border border-gray-200 focus:outline-none hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+              >
+                <svg
+                  aria-hidden="true"
+                  class="mr-1 -ml-1 w-4 h-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                Add member
+              </button>
+              <button
+                type="button"
+                class="inline-flex items-center py-2 px-3 ml-2 text-xs font-medium text-gray-900 bg-white rounded border border-gray-200 sm:ml-0 focus:outline-none hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+              >
+                <svg
+                  aria-hidden="true"
+                  class="mr-1 -ml-1 w-4 h-4"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+                  ></path>
+                </svg>
+                Find a time
+              </button>
+            </div>
+            <div>
+              <label
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >Guest Permissions</label
+              >
+              <div class="space-y-3">
+                <div class="flex items-center mr-4">
+                  <input
+                    id="guest-permission-1-checkbox"
+                    type="checkbox"
+                    value=""
+                    name="guest-permission-checkbox"
+                    class="w-4 h-4 bg-gray-100 rounded border-gray-300 text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  />
+                  <label
+                    for="guest-permission-1-checkbox"
+                    class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                    >Modify event</label
+                  >
+                </div>
+                <div class="flex items-center mr-4">
+                  <input
+                    id="guest-permission-2-checkbox"
+                    type="checkbox"
+                    value=""
+                    name="guest-permission-checkbox"
+                    class="w-4 h-4 bg-gray-100 rounded border-gray-300 text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  />
+                  <label
+                    for="guest-permission-2-checkbox"
+                    class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                    >Invite others</label
+                  >
+                </div>
+                <div class="flex items-center mr-4">
+                  <input
+                    checked
+                    id="guest-permission-3-checkbox"
+                    type="checkbox"
+                    value=""
+                    name="guest-permission-checkbox"
+                    class="w-4 h-4 bg-gray-100 rounded border-gray-300 text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  />
+                  <label
+                    for="guest-permission-3-checkbox"
+                    class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                    >See guest list</label
+                  >
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <button
+          type="submit"
+          class="text-white justify-center bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+        >
+          Add new event
+        </button>
+      </VForm>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { ErrorMessage, Field, Form as VForm } from "vee-validate";
+import * as Yup from "yup";
+interface TradingAcoounts {
+  name: string;
+  broker_id: string;
+  platform_id: string;
+  identity: string;
+  secret?: string;
+  read_only_secret?: string;
+  server: string;
+  link: string;
+  public: boolean;
+}
+const { fetchCategories, categories, fetchMarkets, markets, fetchPlatforms } =
+  useProductStore();
+
+const types = ref<Types[]>([
+  {
+    name: "BOT",
+    value: 13000,
+  },
+  {
+    name: "INDICATOR",
+    value: 13001,
+  },
+  {
+    name: "SYSTEM",
+    value: 13002,
+  },
+  {
+    name: "SCRIPT",
+    value: 13003,
+  },
+  {
+    name: "TEMPLATE",
+    value: 13004,
+  },
+  {
+    name: "ALGORITHM",
+    value: 13005,
+  },
+  {
+    name: "ARTIFICIAL_INTELLIGENCE",
+    value: 13006,
+  },
+  {
+    name: "PORTFOLIO",
+    value: 13007,
+  },
+  {
+    name: "AI_LIVE",
+    value: 13008,
+  },
+  {
+    name: "FUNDED_ACCOUNT",
+    value: 13009,
+  },
+  {
+    name: "TRADING_SIGNAL",
+    value: 13010,
+  },
+  {
+    name: "TRADING_STRATEGY",
+    value: 13011,
+  },
+  {
+    name: "COURSE",
+    value: 13012,
+  },
+]);
+const model = reactive<TradingAcoounts>({} as TradingAcoounts);
+// Validations Account Details
+const form = Yup.object().shape({
+  name: Yup.string().max(128).label("Name"),
+  broker_id: Yup.string().required().label("broker_id"),
+  platform_id: Yup.string().required().label("platform_id"),
+  identity: Yup.string().required().label("identity"),
+  secret: Yup.string()
+    .requiredIf(model.read_only_secret, "Secret Or ReadOnly Secret Required!")
+    .label("Secret"),
+  read_only_secret: Yup.string()
+    .requiredIf(model.read_only_secret, "Secret Or ReadOnly Secret Required!")
+    .required()
+    .label("Introduction"),
+  server: Yup.string().required().label("server"),
+  link: Yup.array().of(Yup.string()).required().file().label("Categories"),
+  public: Yup.boolean().label("public"),
+});
+
+//yu add method check if antoher  field is empty required this field
+Yup.addMethod(Yup.mixed, "requiredIf", function (ref: any, message: string) {
+  return this.test("requiredIf", message, function (value: any) {
+    const otherValue = this.resolve(ref);
+    return !otherValue || !!value;
+  });
+});
+
+Yup.addMethod(Yup.mixed, "file", function () {
+  return this.test("file", "File is required", (value) => {
+    return value && value.length > 0;
+  });
+});
+// Yub add method for file size lower than 5MB
+Yup.addMethod(Yup.mixed, "fileSize", function (size: number) {
+  return this.test("fileSize", "File is too large", (value) => {
+    return value && value[0].size <= size;
+  });
+});
+
+onMounted(() => {
+  nextTick(() => {
+    fetchCategories();
+    fetchMarkets();
+    fetchPlatforms();
+  });
+});
+
+const onFileChange = (e: any) => {
+  var files = e.target.files || e.dataTransfer.files;
+  if (!files.length) return;
+  model.value.logo = files[0];
+  console.log(e.target.files || e.dataTransfer.files);
+};
+const submitForm = async () => {
+  console.log("submitForm", model.value);
+};
+</script>
+
+<style scoped>
+.custom-input-label {
+  @apply block mb-2 text-sm font-medium text-gray-900 dark:text-white;
+}
+.custom-input {
+  @apply bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded outline-none focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500;
+}
+</style>
