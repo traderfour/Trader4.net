ab2f0153ff29d3c9add569bd1c5e0e6c95b213a0
briofy-mammad <m.ranjbar@briofy.net>
2023-04-30 15:15:23 +0330

    remove element plus plugin

--- plugins/modules.client.ts
+++ plugins/modules.client.ts
@@ -1,12 +1,6 @@
 import { Form, Field, ErrorMessage } from "vee-validate";
-import ElementPlus from "element-plus";
-import "element-plus/dist/index.css";
-
 export default defineNuxtPlugin((nuxtApp) => {
-  console.log("Client Plugin INITED");
-
   nuxtApp.vueApp.component("VForm", Form);
   nuxtApp.vueApp.component("VField", Field);
   nuxtApp.vueApp.component("VErrorMessage", ErrorMessage);
-  nuxtApp.vueApp.use(ElementPlus);
 });
