3ca5387929f28b17834d71a492d47a17d4446ddf
briofy-mobinanaseri <m.naseri@briofy.net>
2023-05-08 18:43:30 +0330

    fix slideDrawer

--- components/Pricing/PricingTable.vue
+++ components/Pricing/PricingTable.vue
@@ -3,12 +3,10 @@
     <!-- Headers -->
     <div
       id="table-header"
-      class="hidden md:grid grid-cols-4 gap-x-16 p-4 text-sm font-medium text-gray-900 bg-gray-100 border-t border-b border-gray-200 rounded dark:bg-gray-800 dark:border-gray-700 dark:text-white sticky"
-    >
+      class="hidden md:grid grid-cols-4 gap-x-16 p-4 text-sm font-medium text-gray-900 bg-gray-100 border-t border-b border-gray-200 rounded dark:bg-gray-800 dark:border-gray-700 dark:text-white sticky">
       <div
         v-for="(headerItem, headerIndex) in headers"
-        :key="`header-${headerIndex}`"
-      >
+        :key="`header-${headerIndex}`">
         {{ headerItem.text }}
       </div>
     </div>
@@ -17,12 +15,10 @@
       <div class="overflow-hidden lg:min-w-fit min-w-max">
         <!-- Headers -->
         <div
-          class="md:hidden grid grid-cols-4 gap-x-16 p-4 text-sm font-medium text-gray-900 bg-gray-100 border-t border-b border-gray-200 rounded dark:bg-gray-800 dark:border-gray-700 dark:text-white"
-        >
+          class="md:hidden grid grid-cols-4 gap-x-16 p-4 text-sm font-medium text-gray-900 bg-gray-100 border-t border-b border-gray-200 rounded dark:bg-gray-800 dark:border-gray-700 dark:text-white">
           <div
             v-for="(headerItem, headerIndex) in headers"
-            :key="`header-${headerIndex}`"
-          >
+            :key="`header-${headerIndex}`">
             {{ headerItem.text }}
           </div>
         </div>
@@ -32,29 +28,24 @@
         <div
           v-for="(tableItem, tableIndex) in items"
           :key="tableIndex"
-          class="grid grid-cols-4 gap-x-16 py-5 px-4 text-sm text-gray-700 border-b border-gray-200 dark:border-gray-700 hover:bg-gray-200 rounded dark:hover:bg-slate-700"
-        >
+          class="grid grid-cols-4 gap-x-16 py-5 px-4 text-sm text-gray-700 border-b border-gray-200 dark:border-gray-700 hover:bg-gray-200 rounded dark:hover:bg-slate-700">
           <div
             v-for="(item, idx) in Object.values(tableItem as object)"
-            :key="idx"
-          >
+            :key="idx">
             <!-- if Has ICON -->
             <Icon
               v-if="item.icon"
               :name="item.icon"
-              :class="`text-${item.color} text-2xl`"
-            />
+              :class="`text-${item.color} text-2xl`" />
             <!-- if Has ICON -->
 
             <!-- if Has sub texts array like ["Ticket", "Email (24 Hours)"] -->
             <span
               v-else-if="Array.isArray(item)"
-              class="text-gray-500 dark:text-gray-400"
-            >
+              class="text-gray-500 dark:text-gray-400">
               <span
                 v-for="(subItem, subIndex) in Object.values(item as object)"
-                :key="`sub-${subIndex}`"
-              >
+                :key="`sub-${subIndex}`">
                 {{
                   subItem +
                   (subIndex === Object.values(item as object).length - 1
@@ -71,7 +62,7 @@
               <GlobalSideDrawer
                 v-if="item.hasDrawer && item.hasDrawer === true"
                 :drawer-id="`drawer-${item.text}`"
-              />
+                :items="item" />
               <!-- Side Drawer Component -->
 
               <!-- Tooltip Icon and Text -->
@@ -86,23 +77,20 @@
                   "
                   :aria-controls="
                     item.hasDrawer ? `drawer-${item.text}` : undefined
-                  "
-                >
+                  ">
                   {{ item.text }}
                   <Icon
                     :data-tooltip-target="`tooltip-${item.text}`"
                     type="button"
                     name="mdi:help-circle"
-                    class="outline-none ml-1"
-                  />
+                    class="outline-none ml-1" />
                 </span>
               </div>
 
               <div
                 :id="`tooltip-${item.text}`"
                 role="tooltip"
-                class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
-              >
+                class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
                 {{ item.tooltipText }}
                 <div class="tooltip-arrow" data-popper-arrow></div>
               </div>
@@ -119,8 +107,7 @@
         <!-- Buy Now Buttons -->
         <div
           v-if="hasButtons"
-          class="grid grid-cols-4 gap-x-16 py-5 px-4 text-sm text-gray-700 border-b border-gray-200 dark:border-gray-700"
-        >
+          class="grid grid-cols-4 gap-x-16 py-5 px-4 text-sm text-gray-700 border-b border-gray-200 dark:border-gray-700">
           <div class="text-gray-500 dark:text-gray-400"></div>
           <div>
             <a
@@ -163,6 +150,7 @@ onBeforeMount(() => {
 
   // All Table Headers Array
   const tableHeaders = document.querySelectorAll("#table-header");
+
   tableHeaders.forEach((header: any) => {
     // Style top of header Height
     header.style.top = `${mainHeaderHeight}px`;
