2774d0d9bb8a6ec2f6ae8cc7f92edf0b280e9b75
briofy-parsajiravand <p.jiravand@briofy.net>
2023-05-15 18:22:35 +0330

    setup prev and next button

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -2,34 +2,87 @@
 const { data: navigation } = await useAsyncData("navigation", () =>
   fetchContentNavigation()
 );
+const { path } = useRoute();
+const { data } = await useAsyncData(`content-${path}`, async () => {
+  // fetch document where the document path matches with the cuurent route
+  let article = queryContent().where({ _path: path }).findOne();
+  // get the surround information,
+  // which is an array of documeents that come before and after the current document
+  let surround = queryContent()
+    .only(["_path", "title", "description", "lang"])
+    .sort({ date: 1 })
+    .findSurround(path);
+
+  return {
+    article: await article,
+    surround: await surround,
+  };
+});
+
+const toggleCollapse = (link) => {
+  link.isOpen = !link.isOpen;
+};
+// destrucure `prev` and `next` value from data
+const [prev, next] = data.value?.surround;
 </script>
 <template>
   <main class="prose flex flex-row">
     <!-- create navigation ul with tailwind -->
-    <ul class="flex flex-col w-1/4">
-      <li v-for="link of navigation" :key="link._path" class="py-2">
-        <NuxtLink :to="link._path" class="text-gray-800 hover:text-indigo-500">
-          {{ link.title }}
-          <!-- sub navigation -->
-          <ul class="pl-4 mt-2">
-            <li
-              v-for="sublink of link.children"
-              :key="sublink._path"
-              class="py-1"
+    <ul
+      class="flex flex-col w-1/5 shadow rounded px-2 py-6 m-4 bg-gray-100 dark:bg-gray-800 h-fit"
+    >
+      <li
+        v-for="link of navigation"
+        :key="link._path"
+        class="py-2 dark:text-gray-100"
+      >
+        <div
+          @click="toggleCollapse(link)"
+          class="flex items-center cursor-pointer"
+        >
+          <span class="mr-2">
+            <svg
+              :class="{ 'rotate-90': link.isOpen }"
+              class="w-4 h-4 transition-transform duration-300 transform"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
             >
-              <NuxtLink
-                :to="sublink._path"
-                class="block text-sm text-gray-800 hover:bg-indigo-500 hover:text-white pl-2"
-              >
+              <path d="M9 18l6-6-6-6" />
+            </svg>
+          </span>
+          <span class="text-gray-800 dark:text-gray-100 hover:text-indigo-500">
+            {{ link.title }}
+          </span>
+        </div>
+        <ul v-show="link.isOpen" class="pl-2 mt-2">
+          <li
+            v-for="sublink of link.children"
+            :key="sublink._path"
+            class="py-1"
+          >
+            <NuxtLink
+              :to="sublink._path"
+              class="block text-sm text-gray-800 dark:text-gray-100 pl-2"
+            >
+              <span class="hover:bg-gray-400 hover:text-white px-2 rounded">
                 {{ sublink.title }}
-              </NuxtLink>
-            </li>
-          </ul>
-        </NuxtLink>
+              </span>
+            </NuxtLink>
+          </li>
+        </ul>
       </li>
     </ul>
 
-    <ContentDoc class="basis-2/4" />
+    <div class="basis-2/4">
+      <ContentDoc />
+      <!-- PrevNext Component -->
+      <PrevNext :prev="prev" :next="next" />
+    </div>
 
     <!--     <ContentNavigation v-slot="{ navigation }">
       <ul>
