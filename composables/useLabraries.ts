769ac69358f379e27da3c69efff314a1262bf6eb
briofy-parsajiravand <p.jiravand@briofy.net>
2023-05-22 19:16:51 +0330

    remove useLibraries

--- composables/useLabraries.ts
+++
@@ -1,8 +0,0 @@
-import { reactive } from "vue";
-
-//create libraries with useState nuxt
-export const labraries = useState<string[]>(() => []);
-// set libraries
-export const setLabraries = (data: string[]): void => {
-  labraries.value = data;
-};
