1e6b644d4192b6e622c324a46bcad4b2079337de
briofy-mammad <m.ranjbar@briofy.net>
2023-05-30 21:12:40 +0330

    [ADD] single post and flowbite typo

--- pages/my/posts/create.vue
+++ pages/my/posts/create.vue
@@ -1,141 +1,281 @@
 <template>
-  <section class="bg-white dark:bg-gray-900">
-    <div class="py-8 px-4 mx-auto">
-      <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">
-        Add a new product
-      </h2>
-      <VForm :validation-schema="postSchema" @submit="addPost">
-        <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
-          <div class="sm:col-span-2">
-            <label
-              for="title"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Title
-            </label>
-            <VField
-              v-model="postData.title"
-              type="text"
-              name="title"
-              id="title"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-              placeholder="Type product name"
+  <section class="py-8 bg-white dark:bg-gray-900 lg:py-0">
+    <div class="lg:flex">
+      <div
+        class="hidden w-full max-w-md p-12 lg:h-screen lg:block bg-primary-600"
+      >
+        <div class="flex items-center mb-8 space-x-4">
+          <a
+            href="#"
+            class="flex items-center text-2xl font-semibold text-white"
+          >
+            <img
+              class="w-8 h-8 mr-2"
+              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
             />
-            <VErrorMessage class="text-red-700 text-sm" name="title" />
-          </div>
-          <div class="w-full">
-            <label
-              for="slogan"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            Trader4
+          </a>
+          <a
+            href="#"
+            class="inline-flex items-center text-sm font-medium text-primary-100 hover:text-white"
+          >
+            <svg
+              class="w-6 h-6 mr-1"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              Slogan
-            </label>
-            <input
-              v-model="postData.slogan"
-              type="text"
-              name="slogan"
-              id="slogan"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-              placeholder="Slogan"
-              required
-            />
-          </div>
-          <div class="w-full">
-            <label
-              for="exceprt"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Exceprt
-            </label>
-            <input
-              type="text"
-              name="exceprt"
-              id="exceprt"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-              placeholder="Exceprt"
-            />
-          </div>
-          <div>
-            <label
-              for="public"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-              >Public</label
-            >
-            <select
-              v-model="postData.is_public"
-              id="category"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-            >
-              <option :value="false">Private</option>
-              <option :value="true">Public</option>
-            </select>
-          </div>
-          <div>
-            <label
-              for="public"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Comments
-            </label>
-            <select
-              v-model="postData.comments"
-              id="category"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-            >
-              <option :value="19000">Public</option>
-              <option :value="19001">Private</option>
-              <option :value="19002">Closed</option>
-              <option :value="19003">Subscribers</option>
-            </select>
-          </div>
-
-          <!-- Logo & cover -->
-          <ImageUploader
-            id="logo"
-            key="logo"
-            lable-idle="Logo here"
-            @on-uploaded-successfully="
-              ($event) => {
-                postData.logo_id = $event.results.uuid;
-              }
-            "
-            accepted-file-type="image/jpeg, image/png"
-          />
-
-          <ImageUploader
-            id="cover"
-            key="cover"
-            lable-idle="Cover here"
-            @on-uploaded-successfully="
-              ($event) => {
-                postData.cover_id = $event.results.uuid;
-              }
-            "
-            accepted-file-type="image/jpeg, image/png"
-          />
-          <div class="sm:col-span-2">
-            <ClientOnly>
-              <Editor v-model="postData.content" />
-            </ClientOnly>
-            <span v-if="hasContentError" class="text-red-700 text-sm">
-              Content is Required
-            </span>
-          </div>
+              <path
+                fill-rule="evenodd"
+                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            Go back
+          </a>
         </div>
-        <button
-          type="submit"
-          class="items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white rounded !bg-blue-800 disabled:!bg-gray-500 disabled:cursor-not-allowed"
-          :disabled="loadingDisabled"
-        >
-          <Icon
-            v-if="loadingDisabled"
-            class="animate-spin"
-            size="1.5rem"
-            name="mdi:loading"
-          />
-          <span v-else>Add Post +</span>
-        </button>
-      </VForm>
+        <div class="block p-8 text-white rounded-lg bg-primary-500">
+          <h2 class="mb-1 text-2xl font-semibold">Your selected plan</h2>
+          <p class="mb-4 font-light text-primary-100 sm:text-lg">
+            30-day free trial
+          </p>
+          <!-- List -->
+          <ul role="list" class="space-y-4 text-left">
+            <li class="flex items-center space-x-3">
+              <!-- Icon -->
+              <svg
+                class="flex-shrink-0 w-5 h-5 text-green-300"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+              <span>Individual configuration</span>
+            </li>
+            <li class="flex items-center space-x-3">
+              <!-- Icon -->
+              <svg
+                class="flex-shrink-0 w-5 h-5 text-green-300"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+              <span>No setup, or hidden fees</span>
+            </li>
+            <li class="flex items-center space-x-3">
+              <!-- Icon -->
+              <svg
+                class="flex-shrink-0 w-5 h-5 text-green-300"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+              <span
+                >Team size: <span class="font-semibold">1 developer</span></span
+              >
+            </li>
+            <li class="flex items-center space-x-3">
+              <!-- Icon -->
+              <svg
+                class="flex-shrink-0 w-5 h-5 text-green-300"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+              <span
+                >Premium support:
+                <span class="font-semibold">6 months</span></span
+              >
+            </li>
+            <li class="flex items-center space-x-3">
+              <!-- Icon -->
+              <svg
+                class="flex-shrink-0 w-5 h-5 text-green-300"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+              <span
+                >Free updates: <span class="font-semibold">6 months</span></span
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="flex items-center mx-auto md:w-[42rem] px-4 md:px-8 xl:px-0">
+        <div class="w-full">
+          <div
+            class="flex items-center justify-center mb-8 space-x-4 lg:hidden"
+          >
+            <a href="#" class="flex items-center text-2xl font-semibold">
+              <img
+                class="w-8 h-8 mr-2"
+                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+              />
+              <span class="text-gray-900 dark:text-white">Flowbite</span>
+            </a>
+          </div>
+          <ol
+            class="flex items-center mb-6 text-sm font-medium text-center text-gray-500 dark:text-gray-400 lg:mb-12 sm:text-base"
+          >
+            <li
+              class="flex items-center after:content-[''] after:w-12 after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700"
+            >
+              <div
+                class="flex items-center sm:block after:content-['/'] sm:after:hidden after:mx-2 after:font-light after:text-gray-200 dark:after:text-gray-500"
+              >
+                <div class="mr-2 sm:mb-2 sm:mx-auto">1</div>
+                Personal <span class="hidden sm:inline-flex">Info</span>
+              </div>
+            </li>
+            <li
+              class="flex items-center after:content-[''] after:w-12 after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700"
+            >
+              <div
+                class="flex items-center sm:block after:content-['/'] sm:after:hidden after:mx-2 after:font-light after:text-gray-200 dark:after:text-gray-500"
+              >
+                <div class="mr-2 sm:mb-2 sm:mx-auto">2</div>
+                Account <span class="hidden sm:inline-flex">Info</span>
+              </div>
+            </li>
+            <li class="flex items-center sm:block">
+              <div class="mr-2 sm:mb-2 sm:mx-auto">3</div>
+              Confirmation
+            </li>
+          </ol>
+          <h1
+            class="mb-4 text-2xl font-extrabold leading-tight tracking-tight text-gray-900 sm:mb-6 dark:text-white"
+          >
+            Tell us about yourself
+          </h1>
+          <p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
+            What is your profession?
+          </p>
+          <ul class="mb-6 space-y-4 sm:space-y-6">
+            <li>
+              <input
+                type="radio"
+                id="designer"
+                name="profession"
+                value="designer"
+                class="hidden peer"
+                required
+              />
+              <label
+                for="designer"
+                class="inline-flex items-center justify-center w-full p-5 text-gray-500 border-2 border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-primary-500 peer-checked:border-primary-600 peer-checked:text-primary-600 bg-gray-50 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700"
+              >
+                <svg
+                  class="w-6 h-6 mr-2"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M4 2a2 2 0 00-2 2v11a3 3 0 106 0V4a2 2 0 00-2-2H4zm1 14a1 1 0 100-2 1 1 0 000 2zm5-1.757l4.9-4.9a2 2 0 000-2.828L13.485 5.1a2 2 0 00-2.828 0L10 5.757v8.486zM16 18H9.071l6-6H16a2 2 0 012 2v2a2 2 0 01-2 2z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                <span class="w-full">I’m a Designer</span>
+                <svg
+                  class="w-6 h-6 ml-3"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </label>
+            </li>
+            <li>
+              <input
+                type="radio"
+                id="developer"
+                name="profession"
+                value="developer"
+                class="hidden peer"
+              />
+              <label
+                for="developer"
+                class="inline-flex items-center justify-center w-full p-5 text-gray-500 border-2 border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-primary-500 peer-checked:border-primary-600 peer-checked:text-primary-600 bg-gray-50 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700"
+              >
+                <svg
+                  class="w-6 h-6 mr-2"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M12.316 3.051a1 1 0 01.633 1.265l-4 12a1 1 0 11-1.898-.632l4-12a1 1 0 011.265-.633zM5.707 6.293a1 1 0 010 1.414L3.414 10l2.293 2.293a1 1 0 11-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0zm8.586 0a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 11-1.414-1.414L16.586 10l-2.293-2.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                <span class="w-full">I’m a Developer</span>
+                <svg
+                  class="w-6 h-6 ml-3"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </label>
+            </li>
+          </ul>
+          <button
+            class="w-full px-5 py-2.5 sm:py-3.5 text-sm font-medium text-center text-white rounded-lg bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+          >
+            Next: Account Info
+          </button>
+          <p class="mt-4 text-sm font-light text-gray-500 dark:text-gray-400">
+            Already have an account?
+            <a
+              href="#"
+              class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+              >Login here</a
+            >.
+          </p>
+        </div>
+      </div>
     </div>
   </section>
 </template>
