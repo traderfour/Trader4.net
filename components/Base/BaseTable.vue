5b3f8bfe72922704071198956d78342687f897e1
briofy-mammad <m.ranjbar@briofy.net>
2023-06-02 15:35:14 +0330

    [FIX] table html error

--- components/Base/BaseTable.vue
+++ components/Base/BaseTable.vue
@@ -20,8 +20,6 @@
           >
             <NuxtLink :to="tableButton.link">
               <button
-                id="createProductButton"
-                data-modal-toggle="createProductModal"
                 class="flex items-center rtl:mr-2 ml-2 justify-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
               >
                 <Icon name="mdi:plus" class="me-1" size="16px" />
