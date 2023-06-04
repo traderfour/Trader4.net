3b765aa2b9a8b12e74a36af7c10cc1b3f3b0a66a
briofy-mammad <m.ranjbar@briofy.net>
2023-06-04 16:25:41 +0330

    [ADD] trading plan sub routes

---
+++ pages/my/financial-engineering/trading-plans/strategies/entries/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <BaseTable
+      :table-button="{
+        link: '/create',
+        text: 'Add',
+      }"
+      endpoint="/my/financial-engineering/trading-plans/strategies/entries"
+      :header-filters="headerFilters"
+      title="Trading Plans Strategies"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+const headerFilters = ref([
+  { text: "title", index: 1, key: "title" },
+  { text: "id", index: 1, key: "uuid" },
+]);
+</script>
