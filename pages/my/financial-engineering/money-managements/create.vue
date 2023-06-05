a84aaadf6973ca1c2d99bfe6747de9001db246f7
briofy-mobinanaseri <m.naseri@briofy.net>
2023-06-05 13:54:53 +0330

    fix money management typings

--- pages/my/financial-engineering/money-managements/create.vue
+++ pages/my/financial-engineering/money-managements/create.vue
@@ -41,12 +41,18 @@
               class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
               Position Size Mode
             </label>
-            <input
+            <select
               v-model="moneyData.position_size_mode"
-              type="number"
               name="position_size_mode"
               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-              placeholder="position size mode" />
+              placeholder="position size mode">
+              <option
+                v-for="(risktype, riskIndex) in POSITION_SIZE_MODE"
+                :key="riskIndex"
+                :value="risktype.type">
+                {{ risktype.title }}
+              </option>
+            </select>
           </div>
           <div class="col-span-full lg:col-span-3">
             <label
@@ -54,12 +60,18 @@
               class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
               Position Size Calculation
             </label>
-            <input
+            <select
               v-model="moneyData.position_size_calculation"
-              type="number"
               name="position_size_calculation"
               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-              placeholder="position size calculation" />
+              placeholder="position size calculation">
+              <option
+                v-for="(risktype, riskIndex) in POSITION_SIZE_CALCULATION"
+                :key="riskIndex"
+                :value="risktype.type">
+                {{ risktype.title }}
+              </option>
+            </select>
           </div>
           <div class="col-span-full lg:col-span-3">
             <label
@@ -97,7 +109,12 @@
               v-model="moneyData.status"
               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
               placeholder=" select status">
-              <option>17000</option>
+              <option
+                v-for="(risktype, riskIndex) in STATUS"
+                :key="riskIndex"
+                :value="risktype.type">
+                {{ risktype.title }}
+              </option>
             </select>
           </div>
           <div class="w-full lg:col-span-3 col-span-full">
@@ -144,27 +161,6 @@ import Editor from "@tinymce/tinymce-vue";
 import * as Yup from "yup";
 import { useRiskStore } from "~/store/risk-managements";
 
-// "results": {
-//         "uuid": "994613d4-9511-4e9c-90c8-9e544a7d8546",
-//         "title": "test",
-//         "position_size": "1",
-//         "position_size_mode": 1,
-//         "position_size_calculation": 2,
-//         "maximum_size": 2,
-//         "minimum_size": 2,
-//         "status": 17000,
-//         "trading_account": {
-//             "uuid": "98cbc8e7-b48c-44ca-a1fc-03ed5f01b5c0",
-//             "name": "test name 2",
-//             "status": 15001
-//         },
-//         "instrument": {
-//             "uuid": "91gdc2a2-4251-4619-9bb6-3905d22ber10",
-//             "name": "wwwww",
-//             "status": 12700
-//         }
-//     },
-
 const moneyData = ref({
   title: undefined,
   position_size: undefined,
@@ -172,10 +168,52 @@ const moneyData = ref({
   position_size_calculation: undefined,
   maximum_size: undefined,
   minimum_size: undefined,
-  status: 17000,
+  status: undefined,
   trading_account: {},
   instrument: {},
 });
+const POSITION_SIZE_MODE = [
+  {
+    title: "balance",
+    type: 30200,
+  },
+  {
+    title: "equity",
+    type: 30201,
+  },
+  {
+    title: "free margin",
+    type: 30202,
+  },
+  {
+    title: "margin",
+    type: 30203,
+  },
+];
+const POSITION_SIZE_CALCULATION = [
+  {
+    title: "percentage",
+    type: 30210,
+  },
+  {
+    title: "amount",
+    type: 30211,
+  },
+  {
+    title: "lots",
+    type: 30212,
+  },
+];
+const STATUS = [
+  {
+    title: "active",
+    type: 30220,
+  },
+  {
+    title: "inactive",
+    type: 30221,
+  },
+];
 const hasContentError = ref(false);
 const loadingDisabled = ref(false);
 const store = useRiskStore();
