f132ef725f3a95a2d3342e990568b261d1dcefd7
Muhammad <hi@trader4.net>
2023-06-07 03:41:16 +0100

    [FIX] Fix conflicts bug

--- pages/product/bazaar.vue
+++ pages/product/bazaar.vue
@@ -191,55 +191,6 @@
               <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
                   <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
               </div>
-<<<<<<< HEAD
-              <h3 class="mb-2 text-xl font-bold dark:text-white">Source Feed</h3>
-              <p class="mb-4">
-        <span v-if="readMoreStatus">
-          <p class="font-light text-gray-500 dark:text-gray-400">The Source Feed provides the best spread difference on the market, leading to better outcomes for traders on their chosen platform.</p>
-          </span>
-          <button
-            class="text-blue-500 dark:text-gray-200 hover:underline"
-            @click="readMoreStatus = !readMoreStatus"
-          >
-            {{ readMoreStatus ? "-" : "+" }}
-          </button>
-        </p>
-          </div>
-          <div class="p-6 bg-white rounded shadow dark:bg-gray-800">
-              <div class="flex justify-center items-center mb-4 w-10 h-10 rounded bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-                  <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z"></path></svg>
-              </div>
-              <h3 class="mb-2 text-xl font-bold dark:text-white">Portfolio Management</h3>
-              <p class="mb-4">
-        <span v-if="readMoreStatus">
-          <p class="font-light text-gray-500 dark:text-gray-400">You can create and customize your own investment portfolio from a variety of traders, providing the flexibility to optimize your portfolio to suit your goals and objectives.</p>
-          </span>
-          <button
-            class="text-blue-500 dark:text-gray-200 hover:underline"
-            @click="readMoreStatus = !readMoreStatus"
-          >
-            {{ readMoreStatus ? "-" : "+" }}
-          </button>
-        </p>
-          </div>
-          <div class="p-6 bg-white rounded shadow dark:bg-gray-800">
-              <div class="flex justify-center items-center mb-4 w-10 h-10 rounded bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-                  <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z" clip-rule="evenodd"></path><path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z"></path></svg>                    
-              </div>
-              <h3 class="mb-2 text-xl font-bold dark:text-white">Multi-Market</h3>
-              
-              <p class="mb-4">
-        <span v-if="readMoreStatus">
-          <p class="font-light text-gray-500 dark:text-gray-400">You can even create a personalized portfolio from multiple markets to meet your individual needs.</p>
-          </span>
-          <button
-            class="text-blue-500 dark:text-gray-200 hover:underline"
-            @click="readMoreStatus = !readMoreStatus"
-          >
-            {{ readMoreStatus ? "-" : "+" }}
-          </button>
-        </p>
-=======
               <h3 class="mb-2 text-xl font-bold dark:text-white">Marketing</h3>
               <p class="text-gray-500 dark:text-gray-400">Plan it, create it, launch it. Collaborate seamlessly with all  the organization and hit your marketing goals every month with our marketing plan.</p>
           </div>
@@ -249,28 +200,13 @@
               </div>
               <h3 class="mb-2 text-xl font-bold dark:text-white">Business Automation</h3>
               <p class="text-gray-500 dark:text-gray-400">Auto-assign tasks, send Slack messages, and much more. Now power up with hundreds of new templates to help you get started.</p>
->>>>>>> c44ee06f8796738705f26b0f3648fa854ee321d6
           </div>
           <div>
               <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
                   <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z"></path><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z" clip-rule="evenodd"></path></svg>
               </div>
-<<<<<<< HEAD
-              <h3 class="mb-2 text-xl font-bold dark:text-white">Club</h3>
-        <span v-if="readMoreStatus">
-          <p class="font-light text-gray-500 dark:text-gray-400">Clubs lets traders team up, achieve goals, and celebrate success together. It's a fun way to learn from each other and share strategies.</p>
-          </span>
-          <button
-            class="text-blue-500 dark:text-gray-200 hover:underline"
-            @click="readMoreStatus = !readMoreStatus"
-          >
-            {{ readMoreStatus ? "-" : "+" }}
-          </button>
-        </p>
-=======
               <h3 class="mb-2 text-xl font-bold dark:text-white">Finance</h3>
               <p class="text-gray-500 dark:text-gray-400">Audit-proof software built for critical financial operations like month-end close and quarterly budgeting.</p>
->>>>>>> c44ee06f8796738705f26b0f3648fa854ee321d6
           </div>
           <div>
               <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
@@ -565,9 +501,6 @@ import { initFlowbite } from "flowbite";
 onMounted(() => {
     initFlowbite();
 });
-<<<<<<< HEAD
-const readMoreStatus = ref(false);
-=======
 const features2 = [
   {
     "icon":"mdi:account-multiple",
@@ -608,7 +541,6 @@ const traders = [
     "description":"John Doe is a professional trader with 10 years of experience in the financial markets. He is a specialist in technical analysis and has a Master’s degree in Economics."
   }
 ]
->>>>>>> c44ee06f8796738705f26b0f3648fa854ee321d6
 </script>
 
 
